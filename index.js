5d50ec6de01a442e6f099d4b9f4a1ac4e8bed575
[juseche] <[juseche@motus.com]>
2023-04-05 10:36:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const mustacheExpress = require("mustache-express");
+ 
+// parser for request body
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const port = 3000;
+
+app.engine("html", mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'))
+
+//----------------------------------------- Lab Code Goes Here ----------------------------------------//
+
+
+
+//----------------------------------------- Lab Code Goes Here ----------------------------------------//
